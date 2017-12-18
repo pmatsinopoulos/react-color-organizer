0f937c63f045759f946b9019876f00332e89af5a
Panayotis Matsinopoulos <Panayotis Matsinopoulos panayotis@matsinopoulos.gr>
2017-12-18 20:21:07 +0000

    New sorted by constants

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,9 @@ const constants = {
   RATE_COLOR: "RATE_COLOR",
   REMOVE_COLOR: "REMOVE_COLOR",
   // how colors are sorted:
-  SORTED_BY_DATE: "SORTED_BY_DATE"
+  SORTED_BY_DATE: "SORTED_BY_DATE",
+  SORTED_BY_RATING: "SORTED_BY_RATING",
+  SORTED_BY_TITLE: "SORTED_BY_TITLE"
 };
 
 export default contants;
