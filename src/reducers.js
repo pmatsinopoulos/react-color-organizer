d3dd8ed8f2366fa245a56d705824dd173ce0a107
Panayotis Matsinopoulos <Panayotis Matsinopoulos panayotis@matsinopoulos.gr>
2017-12-18 18:32:24 +0000

    color reducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,7 +1,26 @@
 import C from './constants';
+import {v4} from 'uuid';
 
+// Takes as input the state of a color and an action, e.g. ADD_COLOR.
+// Returns back a new state, that represents a color.
 const color = (state={}, action) => {
-  return {}
+  switch(action.type) {
+    case C.ADD_COLOR:
+      return {
+        id: v4(),
+        title: action.title,
+        color: action.color,
+        timestamp: (new Date()).toUTCString(),
+        rating: 0
+      };
+    case C.RATE_COLOR:
+      return (state.id !== action.id) ? state :   {
+        ...state,
+        rating: action.rating
+      };
+    default:
+      return state;
+  }
 };
 
 const colors = (state=[], action) => {
@@ -12,4 +31,4 @@ const sort = (state=C.SORTED_BY_DATE, action) => {
   return '';
 };
 
-
+export {color};
